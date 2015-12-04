71c37d9d77f65e092958c48958c56e331b612a9e
pskim <pskim1128@gmail.com>
2015-12-04 13:35:58 -0800

    Add various front end links, right before removing g commands adding scaffold

--- Mapper-for-My-Path/app/controllers/users_controller.rb
+++ Mapper-for-My-Path/app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(email: params[:email],
+                      password: params[:password],
+                      authorization: params[:authorization])
+
+    @user.save
+    p "hi"*200
+    p params[:email]
+  end
+
+
 end
